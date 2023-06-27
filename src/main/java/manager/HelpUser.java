a4ea04e47309ab24bec93ddccfb33febb537dcc1
farSEEN21 <58916437+farSEEN21@users.noreply.github.com>
2023-06-27 20:32:15 +0300

    lesson10

--- src/main/java/manager/HelpUser.java
+++ src/main/java/manager/HelpUser.java
@@ -3,7 +3,10 @@ package manager;
 import models.User;
 import models.UserLombok;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -44,26 +47,47 @@ public class HelpUser extends HelperBase {
 //         click(By.xpath("//button[.='Ok']"));
 
     }
-public void fillRegistrationForm(User user){
+
+    public void fillRegistrationForm(User user) {
         type(By.xpath("//input[@id='name']"), user.getName());
         type(By.xpath("//input[@id='lastName']"), user.getLastname());
         type(By.xpath("//input[@id='email']"), user.getEmail());
         type(By.xpath("//input[@id='password']"), user.getPsw());
-        click(By.cssSelector("label[for='terms-of-use']"));
-}
-public void fillRegistrationForm(UserLombok user){
+        clickCheckbox();
+    }
+
+    public void clickCheckbox() {
+        System.out.println("Hello");
+//  var 1  click(By.cssSelector("label[for='terms-of-use']"));
+
+        //var 2      JavascriptExecutor js = (JavascriptExecutor) wd;
+
+//        js.executeScript("document.querySelector('#terms-of-use').click();");
+
+//        var3
+        Rectangle rect = (Rectangle) wd.findElement(By.cssSelector("div.checkbox-container")).getRect();
+        int x = rect.getX() + 5;
+        int y = rect.getY() + rect.getHeight() / 4;
+        Actions actions = new Actions(wd);
+        actions.moveByOffset(x, y).click().perform();
+
+    }
+
+    public void fillRegistrationForm(UserLombok user) {
         type(By.xpath("//input[@id='name']"), user.getName());
         type(By.xpath("//input[@id='lastName']"), user.getLastname());
         type(By.xpath("//input[@id='email']"), user.getEmail());
         type(By.xpath("//input[@id='password']"), user.getPsw());
-        click(By.cssSelector("label[for='terms-of-use']"));
-}
+        //  click(By.cssSelector("label[for='terms-of-use']"));
+        clickCheckbox();
+    }
 //    public void logout() {
 //        wd.findElement(By.xpath("//a[contains(text(),'Logout')]")).click();
 //    }
 
     public void submitLogin() {
         wd.findElement(By.xpath("//button[@type='submit']")).submit();
+//        wd.findElement(By.xpath("//button[@type='submit']")).click();
     }
 
     public void logout() {
--- src/test/java/RegistratoinTest.java
+++ src/test/java/RegistratoinTest.java
@@ -6,46 +6,57 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class RegistratoinTest extends TestBase{
+public class RegistratoinTest extends TestBase {
 
     @BeforeMethod
-    public void precon(){
-        if (app.getUsers().isLogged())app.getUsers().logout();
+    public void precon() {
+        if (app.getUsers().isLogged()) app.getUsers().logout();
     }
 
     @Test
-    public void positiveRegist(){
+    public void positiveRegist() {
 
-        int i=(int)(System.currentTimeMillis()/1000)%3600;
-        User user=new User().withName("Jaffar").withLastname("safw").withEmail("fon"+i+"@gmail.ru").withPsw("$Asdf1234");
+        int i = (int) (System.currentTimeMillis() / 1000) % 3600;
+        User user = new User().withName("Jaffar").withLastname("safw").withEmail("fon" + i + "@gmail.ru").withPsw("$Asdf1234");
 
-app.getUsers().openRegistationForm();
-app.getUsers().fillRegistrationForm(user);
-app.getUsers().submitLogin();
-  Assert.assertTrue(app.getUsers().isLoggedSuccess());
+        app.getUsers().openRegistationForm();
+        app.getUsers().fillRegistrationForm(user);
+        app.getUsers().submitLogin();
+        Assert.assertTrue(app.getUsers().isLoggedSuccess());
     }
 
     @Test
-    public void NegRegist(){
+    public void NegRegist() {
 
-        int i=(int)(System.currentTimeMillis()/1000)%3600;
-        UserLombok userLo=UserLombok.builder().name("Johnny").lastname("shmitt").email("do"+i+"gmail.co").psw("$Asdf1234").build();
+        int i = (int) (System.currentTimeMillis() / 1000) % 3600;
+        UserLombok userLo = UserLombok.builder().name("Johnny").lastname("shmitt").email("do" + i + "gmail.co").psw("$Asdf1234").build();
         app.getUsers().openRegistationForm();
         app.getUsers().fillRegistrationForm(userLo);
         app.getUsers().submitLogin();
         Assert.assertTrue(app.getUsers().isElementPresent(By.xpath("//div[@class='input-container']//div[2]")));
 //        Assert.assertTrue(app.getUsers().isElementPresent(By.xpath("//div[@class='input-container']//div[2]")));
 
+
     }
 
+    @Test
+    public void RegistNegativPassword() {
+
+        int i = (int) (System.currentTimeMillis() / 1000) % 3600;
+        UserLombok userLo = UserLombok.builder().name("Johnny").lastname("shmitt").email("do" + i + "@gmail.co").psw("$asdf1234").build();
+        app.getUsers().openRegistationForm();
+        app.getUsers().fillRegistrationForm(userLo);
+        app.getUsers().submitLogin();
+        Assert.assertTrue(app.getUsers().isElementPresent(By.xpath("//div[@class='input-container']//div[2]")));
+//        Assert.assertTrue(app.getUsers().isElementPresent(By.xpath("//div[@class='input-container']//div[2]")));
 
 
-
+    }
 
     @AfterMethod
-    public void postcond(){
+    public void postcond() {
         app.getUsers().clickOkButton();
-}
+    }
 
 
 }
